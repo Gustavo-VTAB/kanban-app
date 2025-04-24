00278915507c00846ec76cfa035ab294187909b8
Note Ket <gust.inbug@gmail.com>
2025-04-24 03:31:38 -0300

    Refactor web.php to clean up routes and maintain only necessary task routes

--- routes/web.php
+++ routes/web.php
@@ -1,33 +1,23 @@
 <?php
-use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TaskController;
 use App\Http\Controllers\GoogleController;
-
 
 Route::get('/debug-log', function () {
     return response()->file(storage_path('logs/laravel.log'));
 });
 
-
-
 Route::get('/', function () {
     return view('auth.login');
 });
 
-
 Auth::routes();
 
-Route::get('/home', [TaskController::class, 'index'])->name('tasks.index');
-
-Route::resource('tasks', TaskController::class)->middleware('auth');
-
+// Mantenha apenas a rota /tasks com o nome tasks.index
 Route::get('/tasks', [TaskController::class, 'index'])->name('tasks.index');
 
+Route::resource('tasks', TaskController::class)->middleware('auth');
 Route::post('/tasks/update-status', [TaskController::class, 'updateStatus'])->name('tasks.updateStatus');
-
 Route::get('/auth/google', [GoogleController::class, 'redirectToGoogle'])->name('google.auth');
 Route::get('/auth/google/callback', [GoogleController::class, 'handleGoogleCallback']);
-
 Route::post('/tasks/{task}/sync', [TaskController::class, 'syncWithGoogle'])->name('tasks.sync');
-
