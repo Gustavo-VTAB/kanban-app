f9370d957e57b00240f942eca28ba2cc10d8fb73
Note Ket <gust.inbug@gmail.com>
2025-04-24 04:47:54 -0300

    Remove debug log route and clean up task routes in web.php

--- routes/web.php
+++ routes/web.php
@@ -3,21 +3,19 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TaskController;
 use App\Http\Controllers\GoogleController;
 
-Route::get('/debug-log', function () {
-    return response()->file(storage_path('logs/laravel.log'));
-});
-
 Route::get('/', function () {
     return view('auth.login');
 });
 
 Auth::routes();
 
-// Mantenha apenas a rota /tasks com o nome tasks.index
-Route::get('/tasks', [TaskController::class, 'index'])->name('tasks.index');
+Route::get('/home', [TaskController::class, 'index'])->name('home'); // nome diferente!
 
 Route::resource('tasks', TaskController::class)->middleware('auth');
+
 Route::post('/tasks/update-status', [TaskController::class, 'updateStatus'])->name('tasks.updateStatus');
+
+Route::post('/tasks/{task}/sync', [TaskController::class, 'syncWithGoogle'])->name('tasks.sync');
+
 Route::get('/auth/google', [GoogleController::class, 'redirectToGoogle'])->name('google.auth');
 Route::get('/auth/google/callback', [GoogleController::class, 'handleGoogleCallback']);
-Route::post('/tasks/{task}/sync', [TaskController::class, 'syncWithGoogle'])->name('tasks.sync');
